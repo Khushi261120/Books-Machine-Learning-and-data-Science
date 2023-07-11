9e329e69ae60bc0e63d06f6165da259e9ed53565
Khushi261120 <138975978+Khushi261120@users.noreply.github.com>
2023-07-11 21:02:32 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello world")
+
+
+# In[2]:
+
+
+Print("hello world")
+
+
+# In[3]:
+
+
+print(hello world)
+
+
+# In[5]:
+
+
+print(10.7)
+
+
+# In[6]:
+
+
+###Print Hello World
+
+
+# In[7]:
+
+
+8name= khushi
+
+
+# In[8]:
+
+
+###jfuvjvmv
+
+
+# In[9]:
+
+
+###ghbng
+
+
+# ###Data Type
+
+# In[10]:
+
+
+x=5
+type(x)
+
+
+# In[11]:
+
+
+z=True
+type(z)
+
+
+# In[12]:
+
+
+z="khushi"
+type(z)
+
+
+# In[13]:
+
+
+z=khushi
+type(z)
+
+
+# In[14]:
+
+
+x=10
+type(x)
+x
+
+
+# In[15]:
+
+
+x,type(x)
+
+
+# In[16]:
+
+
+x,y=2,4
+x
+y
+
+
+# In[17]:
+
+
+x,y
+
+
+# In[18]:
+
+
+y,x
+
+
+# In[19]:
+
+
+x
+y
+
+
+# ##ffdncnds
+
+# In[20]:
+
+
+x="khushi"
+type(x)
+y="20"
+type(y)
+
+
+# In[22]:
+
+
+y=20
+type(y)
+
+
+# In[ ]:
+
+
+
+
